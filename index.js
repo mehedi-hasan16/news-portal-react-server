1c41522415ed72a4f1dfeeda767aa27baa3f7716
mehedi-hasan16 <hasanmehedi6541@gmail.com>
2023-04-29 16:59:41 +0600

    load data with conditions

--- index.js
+++ index.js
@@ -4,17 +4,38 @@ const cors = require('cors')
 const port = 5000;
 
 const categories = require('./data/categories.json')
+const news = require('./data/news.json')
 
 app.use(cors());
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('news portal is running')
 })
 
-app.get('/categories', (req, res)=>{
+app.get('/categories', (req, res) => {
     res.send(categories)
 })
 
-app.listen(port, ()=>{
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(news => news._id === id);
+    res.send(selectedNews)
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id;
+    if (id == 0) {
+        res.send(news)
+    } else {
+        const categoryNews = news.filter(cNews => cNews.category_id == id);
+        res.send(categoryNews)
+    }
+})
+
+app.listen(port, () => {
     console.log('news poral is running on port', port);
 })
